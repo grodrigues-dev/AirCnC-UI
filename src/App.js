646768953423f5ffc57f5412517301070a071ac5
grodrigues-dev <52932384+grodrigues-dev@users.noreply.github.com>
2019-12-14 11:30:14 -0300

    Utilizando axios para consumir a api

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
+import api from './services/api'
 
 import logo from './assets/logo.svg';
 
+
 function App() {
+const [email, setEmail] = useState('');
+  async function handleSubmit(event){
+    event.preventDefault();
+     const response = await api.post('/sessions', {email}); 
+      console.log(response);
+      
+  }
   return (
     <div className="container">
       <img src={logo} alt=""/>
       <div className="content">
-   
         <p>Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa</p>
-        <form >
+        <form onSubmit ={handleSubmit} >
           <label htmlFor="email">E-MAIL *</label>
           <input 
           type="email"
           id="email" 
           placeholder="Digite aqui o seu e-mail"
+          value={email}
+          onChange ={event => setEmail(event.target.value)}
           />
           <button type="submit" className="btn"> Entrar</button>
         </form>
