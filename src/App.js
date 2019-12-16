786cebb4b73a498b2fcf6abbdf4f5864b693543d
grodrigues-dev <52932384+grodrigues-dev@users.noreply.github.com>
2019-12-16 10:30:03 -0300

    Adicionando rotas

--- src/App.js
+++ src/App.js
@@ -1,34 +1,16 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './App.css';
-import api from './services/api'
 
 import logo from './assets/logo.svg';
 
+import Routes from './routes'; 
 
 function App() {
-const [email, setEmail] = useState('');
-  async function handleSubmit(event){
-    event.preventDefault();
-     const response = await api.post('/sessions', {email}); 
-      console.log(response);
-      
-  }
   return (
     <div className="container">
       <img src={logo} alt=""/>
       <div className="content">
-        <p>Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa</p>
-        <form onSubmit ={handleSubmit} >
-          <label htmlFor="email">E-MAIL *</label>
-          <input 
-          type="email"
-          id="email" 
-          placeholder="Digite aqui o seu e-mail"
-          value={email}
-          onChange ={event => setEmail(event.target.value)}
-          />
-          <button type="submit" className="btn"> Entrar</button>
-        </form>
+        <Routes />
       </div>
     </div>
   );
