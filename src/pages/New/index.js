e752799eb5ed37918e51493f84c87cb7bcde279d
grodrigues-dev <52932384+grodrigues-dev@users.noreply.github.com>
2019-12-16 13:54:59 -0300

    Adicionando tela de cadastro de spots

--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -1,5 +1,36 @@
-import React from 'react'; 
+import React, {useState} from 'react'; 
 
 export default function New(){
-    return <div />
+    const [company, setCompany] = useState('');
+    const [techs, setTechs] = useState('');
+    const [price, setPrice] = useState('');
+    function handleSubmit(){
+
+    }
+    return (
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="company">EMPRESA *</label>
+            <input 
+                id="company"
+                placeholder="Sua empresa"
+                value={company}
+                onChange={event => setCompany(event.target.value)}
+            />
+            <label htmlFor="techs">TECNOLOGIAS *<span>(separadas por vírgula)</span></label>
+            <input 
+                id="techs"
+                placeholder="Quais tecnologias usam?"
+                value={techs}
+                onChange={event => setTechs(event.target.value)}
+            />
+            <label htmlFor="price">VALOR DA DIÁRIA *<span>(em branco  para GRATUITO)</span></label>
+            <input 
+                id="price"
+                placeholder="Valor cobrado por dia"
+                value={price}
+                onChange={event => setPrice(event.target.value)}
+            />
+            <button type="submit" className="btn">Cadastrar</button>
+        </form>
+    )
 }
