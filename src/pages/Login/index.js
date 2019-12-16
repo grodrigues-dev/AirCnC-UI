5f7c1c2a523902fa9860de70c604ffc1ec062def
grodrigues-dev <52932384+grodrigues-dev@users.noreply.github.com>
2019-12-16 10:54:46 -0300

    Consultando spots por usuário

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -2,13 +2,14 @@ import React, {useState} from 'react';
 import api from '../../services/api'
 
 
-export default function Login() {
+export default function Login({ history}) {
     const [email, setEmail] = useState('');
     async function handleSubmit(event) {
         event.preventDefault();
         const response = await api.post('/sessions', { email });
         const { _id } = response.data;
         localStorage.setItem('user', _id)
+        history.push('/dashboard')
     }
     return (
         <>
