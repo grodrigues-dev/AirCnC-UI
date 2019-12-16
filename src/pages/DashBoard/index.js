107bc330e2ca2fc9d8b2195fc059c5a49b5e605a
grodrigues-dev <52932384+grodrigues-dev@users.noreply.github.com>
2019-12-16 11:56:29 -0300

    Estilizando tela de dashboard

--- src/pages/DashBoard/index.js
+++ src/pages/DashBoard/index.js
@@ -1,18 +1,40 @@
-import React, {useEffect} from 'react'; 
+import React, {useEffect, useState} from 'react'; 
 import api  from '../../services/api'
+import {Link} from 'react-router-dom';
+import './style.css'
+
 
 export default function Dashboard(){
+    const [spots, setSpots] = useState([]);
     useEffect(() => {
         async function loadSpots(){
             const user_id = localStorage.getItem('user');
             const response = await api.get('/dashboard', {
                 headers: {user_id}
             }); 
-            console.log(response.data);
+            setSpots(response.data)
             
         }
         loadSpots();
     }, []);
-    return <h1>Dashboard</h1>
+    return (
+        <>
+        <ul className="spot-list">
+            {spots.map(spot => (
+                <li key= {spot._id}>
+                    <header style={{backgroundImage: `url(${spot.thumbnail_url})`}} />
+                    <strong>{spot.company}</strong>
+                    <span>{(spot.price || spot.price > 0 ) ? `R$${spot.price}` : 'GRATUITO' }</span>
+                </li>
+            ))}
+        </ul>
+        <Link to="/new"> 
+                <button className="btn">
+                Cadastrar novo spot
+                </button>
+                
+        </Link>
+        </>
+    )
 
 }
