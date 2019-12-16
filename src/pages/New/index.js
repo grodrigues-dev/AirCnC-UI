83b3604b1d2ff17a81466e4c866b29efdd5347cb
grodrigues-dev <52932384+grodrigues-dev@users.noreply.github.com>
2019-12-16 14:42:03 -0300

    Criando preview

--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -1,19 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo} from 'react';
 import camera from '../../assets/camera.svg'
 import './style.css'
 
 export default function New() {
+    const [thumbnail, setThumbnail] = useState(null);
     const [company, setCompany] = useState('');
     const [techs, setTechs] = useState('');
     const [price, setPrice] = useState('');
+
+    const preview = useMemo(
+        () => {
+            return thumbnail ? URL.createObjectURL(thumbnail) : null;
+        }, [thumbnail]
+    )
+
     function handleSubmit() {
 
     }
     return (
         <form onSubmit={handleSubmit}>
-            <label id="thumbnail">
-                <input type="file"/>
-                <img src={camera} alt="Select image"/>
+            <label 
+            id="thumbnail" 
+            style= {{backgroundImage: `url(${preview})`}}
+            className= { thumbnail ? 'has-thumbnail' : ''}
+            >
+                <input 
+                type="file"
+                onChange={event => setThumbnail(event.target.files[0])}/>
+                <img src={camera} alt="Select img"/>
             </label>
 
             <label htmlFor="company">EMPRESA *</label>
