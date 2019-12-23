920dc00ca1094f20da8991b5a5550d5acac254f2
grodrigues-dev <52932384+grodrigues-dev@users.noreply.github.com>
2019-12-23 10:56:13 -0300

    Adicionando websocket

--- src/pages/DashBoard/index.js
+++ src/pages/DashBoard/index.js
@@ -1,11 +1,19 @@
 import React, {useEffect, useState} from 'react'; 
 import api  from '../../services/api'
 import {Link} from 'react-router-dom';
+import socketio from 'socket.io-client'
 import './style.css'
 
 
 export default function Dashboard(){
     const [spots, setSpots] = useState([]);
+
+    useEffect(()=>{
+        const socket = socketio('http://localhost:3001'); 
+        socket.emit("msg", "Novas Conexões")
+        
+    },[])
+
     useEffect(() => {
         async function loadSpots(){
             const user_id = localStorage.getItem('user');
