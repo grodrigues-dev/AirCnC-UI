e07f23d2d58308bf3bde1d0a449dee808ecce5b6
grodrigues-dev <52932384+grodrigues-dev@users.noreply.github.com>
2019-12-23 12:22:03 -0300

    Recebendo notificações em tempo real

--- src/pages/DashBoard/index.js
+++ src/pages/DashBoard/index.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react'; 
+import React, {useEffect, useState, useMemo} from 'react'; 
 import api  from '../../services/api'
 import {Link} from 'react-router-dom';
 import socketio from 'socket.io-client'
@@ -7,15 +7,20 @@ import './style.css'
 
 export default function Dashboard(){
     const [spots, setSpots] = useState([]);
+    const [requests, setRequest] = useState([]);
+
+    const user_id = localStorage.getItem('user');
+    const socket = useMemo(()=>socketio('http://localhost:3001', {
+       query: {
+           user_id
+       } 
+    }),[user_id]);   
 
     useEffect(()=>{
-        const user_id = localStorage.getItem('user');
-        const socket = socketio('http://localhost:3001', {
-           query: {
-               user_id
-           } 
-        });         
-    },[])
+        socket.on('bookingRequest', data=>{
+            setRequest([...requests, data]);
+        })      
+    },[requests, socket]);
 
     useEffect(() => {
         async function loadSpots(){
@@ -30,6 +35,18 @@ export default function Dashboard(){
     }, []);
     return (
         <>
+        <ul className="notifications">
+            {requests.map(request =>(
+                <li key={request._id}>
+                    <p>
+                        <strong>{request.user.email}</strong> está solicitando uma reserva em 
+                        <strong>{request.spot.company}</strong> para da data: <strong> {request.date}</strong>
+                        <button>Aceitar</button>
+                        <button>Rejeitar</button>
+                    </p>
+                </li>
+            ))}
+        </ul>
         <ul className="spot-list">
             {spots.map(spot => (
                 <li key= {spot._id}>
